bc983b902f5ae38b41eb2991358e66f084426db3
Fabio Galante Mans <fabiogalantemans@gmail.com>
2022-05-21 17:59:16 -0300

    Reading files in project

--- FilesApi/Controllers/FilesController.cs
+++ FilesApi/Controllers/FilesController.cs
@@ -4,12 +4,12 @@ namespace FilesApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class FilesController : ControllerBase
 {
     
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<FilesController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public FilesController(ILogger<FilesController> logger)
     {
         _logger = logger;
     }
@@ -17,7 +17,21 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public ActionResult Get()
     {
-        var file = "Galante";
-        return Ok(file);
+
+        string guid;
+        string currentDirectory = Directory.GetCurrentDirectory();
+        
+        _logger.LogInformation($"Current Directory {currentDirectory}");
+        string path = "Cripto";
+        string fullPath = Path.Combine(currentDirectory, path, "Chave.txt");
+        
+        _logger.LogInformation($"FullPath {fullPath}");
+        using (StreamReader r = new StreamReader(fullPath))
+        { 
+            guid = r.ReadToEnd();
+            
+        }
+        
+        return Ok(guid);
     }
 }
